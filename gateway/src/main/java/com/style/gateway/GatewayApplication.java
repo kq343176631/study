4f7e0dacb9387ce086b1defc7363fd1d0c715276
维丶强 <343176631@qq.com>
2019-02-20 22:26:05 +0800

    Init Commit

---
+++ gateway/src/main/java/com/style/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package com.style.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Gateway Application
+ * @author ThinkGem
+ * @version 2018-10-13
+ */
+@SpringBootApplication
+public class GatewayApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+	
+}
