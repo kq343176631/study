da6beb70446426934bb4c56a59d35778a2a6edc7
kq343176631 <kq343176631@163.com>
2019-03-31 02:36:38 +0800

    Default

--- utils/src/main/java/com/style/utils/core/GlobalUtils.java
+++ utils/src/main/java/com/style/utils/core/GlobalUtils.java
@@ -10,25 +10,16 @@ import java.util.Map;
 
 /**
  * 全局工具类
- *
- * @version 2018-11-11
  */
 public class GlobalUtils {
 
     private static Logger logger = LoggerFactory.getLogger(GlobalUtils.class);
-
-    private static final String DATA_SOURCE_KEY_PREFIX = "target-datasource";
-
-    private static final String DATA_SOURCE_NAME_KEY_PREFIX = "target-datasource.datasource-name";
 
     // 缓存全局属性
     private static Map<String, Object> props = MapUtils.newLinkedHashMap();
 
     /**
      * 获取属性，如果globalProperty中没有，则从PropertiesUtil获取属性值。
-     *
-     * @param key key
-     * @return String
      */
     public static String getProperty(String key) {
         String value = null;
@@ -45,77 +36,9 @@ public class GlobalUtils {
         return value;
     }
 
-    public static String getText(String s) {
-        return null;
-    }
-
-    public static String getAdminPath() {
-
-        return null;
-    }
-
     public String getProperty(String key, String defaultValue) {
         String value = getProperty(key);
         return value != null ? value : defaultValue;
-    }
-
-    /**
-     * 检查系统是否启用动态数据源
-     *
-     * @return boolean
-     */
-    public static boolean isEnableDynamicDataSource() {
-        String dataSourceNames = GlobalUtils.getProperty(DATA_SOURCE_NAME_KEY_PREFIX);
-        return (StringUtils.isNotBlank(dataSourceNames) && dataSourceNames.split(",").length > 1);
-    }
-
-    /**
-     * 获取默认的数据源名称
-     *
-     * @return dataSourceName
-     */
-    public static String getDefaultDataSourceName() {
-        String[] dataSourceNames = getProperty(DATA_SOURCE_NAME_KEY_PREFIX).split(",");
-        String result = null;
-        for (String dataSourceName : dataSourceNames) {
-            String dataSourceType = getProperty(DATA_SOURCE_KEY_PREFIX
-                    + "."
-                    + dataSourceName
-                    + "."
-                    + "default");
-            if ("true".equals(dataSourceType)) {
-                result = dataSourceName;
-            }
-        }
-        if (StringUtils.isBlank(result)) {
-            //throw new SystemException("默认数据源名称不能为空。。。");
-        }
-        return result;
-    }
-
-    /**
-     * 获取目标数据源名称
-     *
-     * @return targetDataSourceNames 数组的第一个位置放默认数据源，剩下位置放其它数据源。
-     */
-    public static String[] getTargetDataSourceNames() {
-        String[] dataSourceNames = getProperty(DATA_SOURCE_NAME_KEY_PREFIX).split(",");
-        String[] targetDataSourceNames = new String[dataSourceNames.length];
-        int otherDataSourceIndex = 1;
-        for (String dataSourceName : dataSourceNames) {
-            String dataSourceType = getProperty(DATA_SOURCE_KEY_PREFIX
-                    + "."
-                    + dataSourceName
-                    + "."
-                    + "default");
-            if ("true".equals(dataSourceType)) {
-                targetDataSourceNames[0] = dataSourceName;
-            } else {
-                targetDataSourceNames[otherDataSourceIndex] = dataSourceName;
-            }
-
-        }
-        return targetDataSourceNames;
     }
 
 }
--- utils/src/main/java/com/style/utils/io/PropertyUtils.java
+++ utils/src/main/java/com/style/utils/io/PropertyUtils.java
@@ -27,6 +27,8 @@ import java.util.regex.Pattern;
 @SuppressWarnings("all")
 public class PropertyUtils {
 
+    private static Logger logger = PropertyUtils.initLogger();
+
     /**
      * 默认加载的文件，可通过继承覆盖（若有相同Key，优先加载后面的）
      */
@@ -36,18 +38,145 @@ public class PropertyUtils {
             "classpath:config/application.yml",
             "classpath:application.yml"};
 
-    private static Logger logger = PropertyUtils.initLogger();
-
     private final Set<String> configSet = SetUtils.newLinkedHashSet();
+
+    private static Environment environment;
 
     private final Properties properties = new Properties();
 
-    private static Environment environment;
+    /**
+     * 载入多个文件，路径使用Spring Resource格式，相同的属性在最后载入的文件中的值将会覆盖之前的值。
+     */
+    public PropertyUtils(String... configFiles) {
+        for (String location : configFiles) {
+            try {
+                Resource resource = ResourceUtils.getResource(location);
+                if (resource.exists()) {
+                    if (location.endsWith(".properties")) {
+                        try (InputStreamReader is = new InputStreamReader(resource.getInputStream(), "UTF-8")) {
+                            properties.load(is);
+                            configSet.add(location);
+                        } catch (IOException ex) {
+                            logger.error("Load " + location + " failure. ", ex);
+                        }
+                    } else if (location.endsWith(".yml")) {
+                        YamlPropertiesFactoryBean bean = new YamlPropertiesFactoryBean();
+                        bean.setResources(resource);
+                        for (Map.Entry<Object, Object> entry : bean.getObject().entrySet()) {
+                            properties.put(ObjectUtils.toString(entry.getKey()),
+                                    ObjectUtils.toString(entry.getValue()));
+                        }
+                        configSet.add(location);
+                    }
+                }
+            } catch (Exception e) {
+                logger.error("Load " + location + " failure. ", e);
+            }
+        }
+    }
+
+    // 正则表达式预编译
+    private static Pattern p1 = Pattern.compile("\\$\\{.*?\\}");
+
+    /**
+     * 获取属性值，取不到从System.getProperty()获取，都取不到返回null
+     */
+    public String getProperty(String key) {
+        if (environment != null) {
+            String value = environment.getProperty(key);
+            if (value != null) {
+                return value;
+            }
+        }
+        String value = properties.getProperty(key);
+        if (value != null) {
+            Matcher m = p1.matcher(value);
+            while (m.find()) {
+                String g = m.group();
+                String childKey = g.replaceAll("\\$\\{|\\}", "");
+                value = StringUtils.replace(value, g, getProperty(childKey));
+            }
+            return value;
+        } else {
+            String systemProperty = System.getProperty(key);
+            if (systemProperty != null) {
+                return systemProperty;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 取出String类型的Property，但以System的Property优先，如果都为null则返回defaultValue值
+     */
+    public String getProperty(String key, String defaultValue) {
+        String value = getProperty(key);
+        return value != null ? value : defaultValue;
+    }
+
+    /**
+     * 初始化日志路径
+     */
+    private static Logger initLogger() {
+        String logPath = null;
+        try {
+            // 获取当前classes目录
+            logPath = new DefaultResourceLoader().getResource("/").getFile().getPath();
+        } catch (Exception e) {
+            // 取不到，取当前工作路径
+            logPath = System.getProperty("user.dir");
+        }
+        // 取当前日志路径下有classes目录，则使用classes目录
+        String classesLogPath = FileUtils.path(logPath + "/WEB-INF/classes");
+        if (new File(classesLogPath).exists()) {
+            logPath = classesLogPath;
+        }
+        if (StringUtils.isBlank(System.getProperty("logPath"))) {
+            System.setProperty("logPath", FileUtils.path(logPath));
+        }
+        return LoggerFactory.getLogger(PropertyUtils.class);
+    }
+
+    /**
+     * 获取当前加载的属性文件
+     */
+    public Set<String> getConfigSet() {
+        return configSet;
+    }
+
+    /**
+     * 获取当前加载的属性数据
+     */
+    public Properties getProperties() {
+        return properties;
+    }
+
+    /**
+     * 当前类实例
+     */
+    public static PropertyUtils getInstance() {
+        return PropertiesLoaderHolder.INSTANCE;
+    }
+
+    /**
+     * 重新加载实例（重新实例化，以重新加载属性文件数据）
+     */
+    public static void releadInstance() {
+        PropertiesLoaderHolder.releadInstance();
+    }
+
+    /**
+     * 设置环境属性
+     */
+    public static void setEnvironment(Environment environment) {
+        PropertyUtils.environment = environment;
+    }
 
     /**
      * 当前类的实例持有者（静态内部类，延迟加载，懒汉式，线程安全的单例模式）
      */
     private static final class PropertiesLoaderHolder {
+
         private static PropertyUtils INSTANCE;
 
         static {
@@ -104,133 +233,4 @@ public class PropertyUtils {
         }
     }
 
-    /**
-     * 载入多个文件，路径使用Spring Resource格式，相同的属性在最后载入的文件中的值将会覆盖之前的值。
-     */
-    public PropertyUtils(String... configFiles) {
-        for (String location : configFiles) {
-            try {
-                Resource resource = ResourceUtils.getResource(location);
-                if (resource.exists()) {
-                    if (location.endsWith(".properties")) {
-                        try (InputStreamReader is = new InputStreamReader(resource.getInputStream(), "UTF-8")) {
-                            properties.load(is);
-                            configSet.add(location);
-                        } catch (IOException ex) {
-                            logger.error("Load " + location + " failure. ", ex);
-                        }
-                    } else if (location.endsWith(".yml")) {
-                        YamlPropertiesFactoryBean bean = new YamlPropertiesFactoryBean();
-                        bean.setResources(resource);
-                        for (Map.Entry<Object, Object> entry : bean.getObject().entrySet()) {
-                            properties.put(ObjectUtils.toString(entry.getKey()),
-                                    ObjectUtils.toString(entry.getValue()));
-                        }
-                        configSet.add(location);
-                    }
-                }
-            } catch (Exception e) {
-                logger.error("Load " + location + " failure. ", e);
-            }
-        }
-    }
-
-    /**
-     * 获取当前加载的属性文件
-     */
-    public Set<String> getConfigSet() {
-        return configSet;
-    }
-
-    /**
-     * 获取当前加载的属性数据
-     */
-    public Properties getProperties() {
-        return properties;
-    }
-
-    /**
-     * 当前类实例
-     */
-    public static PropertyUtils getInstance() {
-        return PropertiesLoaderHolder.INSTANCE;
-    }
-
-    /**
-     * 重新加载实例（重新实例化，以重新加载属性文件数据）
-     */
-    public static void releadInstance() {
-        PropertiesLoaderHolder.releadInstance();
-    }
-
-    // 正则表达式预编译
-    private static Pattern p1 = Pattern.compile("\\$\\{.*?\\}");
-
-    /**
-     * 获取属性值，取不到从System.getProperty()获取，都取不到返回null
-     */
-    public String getProperty(String key) {
-        if (environment != null) {
-            String value = environment.getProperty(key);
-            if (value != null) {
-                return value;
-            }
-        }
-        String value = properties.getProperty(key);
-        if (value != null) {
-            Matcher m = p1.matcher(value);
-            while (m.find()) {
-                String g = m.group();
-                String childKey = g.replaceAll("\\$\\{|\\}", "");
-                value = StringUtils.replace(value, g, getProperty(childKey));
-            }
-            return value;
-        } else {
-            String systemProperty = System.getProperty(key);
-            if (systemProperty != null) {
-                return systemProperty;
-            }
-        }
-        return null;
-    }
-
-    /**
-     * 取出String类型的Property，但以System的Property优先，如果都为null则返回defaultValue值
-     */
-    public String getProperty(String key, String defaultValue) {
-        String value = getProperty(key);
-        return value != null ? value : defaultValue;
-    }
-
-    /**
-     * 设置环境属性
-     *
-     * @param environment
-     */
-    public static void setEnvironment(Environment environment) {
-        PropertyUtils.environment = environment;
-    }
-
-    /**
-     * 初始化日志路径
-     */
-    private static Logger initLogger() {
-        String logPath = null;
-        try {
-            // 获取当前classes目录
-            logPath = new DefaultResourceLoader().getResource("/").getFile().getPath();
-        } catch (Exception e) {
-            // 取不到，取当前工作路径
-            logPath = System.getProperty("user.dir");
-        }
-        // 取当前日志路径下有classes目录，则使用classes目录
-        String classesLogPath = FileUtils.path(logPath + "/WEB-INF/classes");
-        if (new File(classesLogPath).exists()) {
-            logPath = classesLogPath;
-        }
-        if (StringUtils.isBlank(System.getProperty("logPath"))) {
-            System.setProperty("logPath", FileUtils.path(logPath));
-        }
-        return LoggerFactory.getLogger(PropertyUtils.class);
-    }
 }
